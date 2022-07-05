8c4ba7f7bd38148430042a5c1479c04207c43b58
Yuliana <yulianamarcela200@gmail.com>
2022-07-05 09:47:46 -0400

    Pasando contenido con EJS a ABOUT y CONTACT

--- app.js
+++ app.js
@@ -21,7 +21,12 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.render("home", { contentHome: homeStartingContent });
 });
-
+app.get("/about", function(req,res){
+  res.render("about", {contentAbout: aboutContent});
+})
+app.get("/contact", function(req,res){
+  res.render("contact", {contentContact: contactContent});
+})
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
