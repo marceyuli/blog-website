4fb5afbe881ccf2f99a0490b6df97a9ca3472691
Yuliana Montaño <yulianamarcela200@gmail.com>
2022-08-17 16:50:02 +0200

    Daily journal using ejs dra yu course

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -12,6 +13,7 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+var posts = [];
 
 app.set("view engine", "ejs");
 
@@ -19,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { contentHome: homeStartingContent });
+  res.render("home", { contentHome: homeStartingContent , allThePosts: posts});
 });
 app.get("/about", function (req, res) {
   res.render("about", { contentAbout: aboutContent });
@@ -31,7 +33,24 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 app.post("/compose", function (req, res) {
-  console.log(req.body.contentOfAPost);
+  const post = {
+    title: req.body.titleOfAPost,
+    content: req.body.contentOfAPost,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+app.get("/posts/:title", function(req,res){
+  const requestedTitle =  _.lowerCase(req.params.title);
+  posts.forEach(post => {
+    if(_.lowerCase(post.title) == requestedTitle){ 
+      const title = post.title;
+      const content= post.content;
+      res.render("post", {theTitle: title, theContent: content });
+    }
+  });
+
+
 });
 app.listen(3000, function () {
   console.log("Server started on port 3000");
